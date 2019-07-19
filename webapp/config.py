52ee82b6103ae0780b8e1ff59935bb5fe581cd9b
Abhinav Jain <abhinav.jain@flipkart.com>
2019-07-19 11:21:25 +0530

    add common word sets

--- webapp/config.py
+++ webapp/config.py
@@ -2,3 +2,5 @@ MODEL_PATH="../models/langdetect_augmented.bin"
 
 HINDI_DICTIONARY_PATH = '../datasets/hindi_profane_words.csv'
 ENG_DICTIONARY_PATH = '../datasets/english_profane_words.csv'
+
+COMMON_WORD_DICTIONARY='../datasets/google-10000-english-no-swears.txt'
--- webapp/profanity.py
+++ webapp/profanity.py
@@ -1,5 +1,5 @@
 from fuzzywuzzy import fuzz
-from config import HINDI_DICTIONARY_PATH, ENG_DICTIONARY_PATH
+from config import HINDI_DICTIONARY_PATH, ENG_DICTIONARY_PATH, COMMON_WORD_DICTIONARY
 
 THRESHOLD_DEFAULT = 50
 LANGUAGE_DEFAULT = "hi"
@@ -7,9 +7,15 @@ LANGUAGE_DEFAULT = "hi"
 profane_dict = {"hi": HINDI_DICTIONARY_PATH, "en": ENG_DICTIONARY_PATH}
 profane_words = {}
 
+common_word_set = set()
+
 for lang, filename in profane_dict.items():
     with open(filename) as file:
         profane_words[lang] = [line.rstrip('\n') for line in file]
+
+with open(COMMON_WORD_DICTIONARY) as file:
+    for line in file:
+        common_word_set.add(line.rstrip('\n'))
 
 
 def get_profanity_score(text,
@@ -18,6 +24,8 @@ def get_profanity_score(text,
     profane_map = {}
     for word in text.split(" "):
         # profane_map = {}
+        if lang == 'en' and word in common_word_set:
+            continue
         for profane_word in profane_words[lang]:
             match = fuzz.ratio(word, profane_word)
             # match = fuzz.token_set_ratio(text, profane_word)
@@ -28,4 +36,5 @@ def get_profanity_score(text,
     return profane_map
 
 # if __name__ == '__main__':
-# print(get_profanity_score(""))
+#     print(get_profanity_score("review goes here", 70, "en"))
+    # print(common_word_set)
